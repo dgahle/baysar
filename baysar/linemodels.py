Completed functional capacity of the background and intensity and wavelength calibration. Added BowmanTee distribution based plasma profile function. Fixed bug in calculation of impurity electrons (error in extrapolation outside of the self.adas_plasma_inputs bounds). Some improvement of the module structure of adding tools.py to store supporting functions that are used by multiple baysar classes and output_tools.py for supporting handling of samples produced from using the baysar posterior.

baysar/linemodels.py
@@ -62,6 +62,7 @@ class DopplerLine(object):
 
     def __call__(self, ti, ems):
         fwhm=self.line.cwl*7.715e-5*sqrt(ti/self.atomic_mass)
+        # print(self.line.cwl, self.atomic_mass, fwhm, ti)
         return self.line([fwhm, ems])
 
 
@@ -148,7 +149,7 @@ class ADAS406Lines(object):
         tau = self.plasma.plasma_state[self.species+'_tau'][0]
         # tau = np.zeros(len(ne))+self.plasma.plasma_state[self.species+'_tau'][0]
 
-        # self.tec_in[:, 0] = self.plasma.plasma_state[self.species+'_tau'][0]# np.array([tau, ne, te])
+        self.tec_in[:, 0] = self.plasma.plasma_state[self.species+'_tau'][0]# np.array([tau, ne, te])
         self.tec_in[:, 1] = self.plasma.plasma_state['electron_density']
         self.tec_in[:, 2] = self.plasma.plasma_state['electron_temperature']
         # TODO: comparison to weight on log(tau) vs tau
@@ -250,13 +251,22 @@ def zeeman_split(cwl, peak, wavelengths, b_field, viewangle):
     ls_pi = rel_intensity_pi * peak
     return ls_sigma_minus + ls_pi + ls_sigma_plus
 
+from copy import copy
+
 class HydrogenLineShape(object):
     def __init__(self, cwl, wavelengths, n_upper, n_lower, atomic_mass, zeeman=True):
         self.cwl = cwl
         self.wavelengths = wavelengths
         self.zeeman = zeeman
-        self.n_upper=n_upper
-        self.n_lower=n_lower
+        if int(n_lower)==1:
+            self.n_upper=n_upper+1
+            self.n_lower=2
+            UserWarning("Using the Balmer Stark shape coefficients for the Lyman series. Transition n=%d -> %d | %f A"%(self.n_upper, self.n_lower, np.round(self.cwl, 2)))
+        else:
+            self.n_upper=n_upper
+            self.n_lower=n_lower
+
+        # print("Transition n=%d -> %d | %f A"%(self.n_upper, self.n_lower, np.round(self.cwl, 2)))
 
         # TODO - why?
         wavelengths_doppler_num = len(self.wavelengths) # todo make a power of 2
@@ -264,7 +274,7 @@ class HydrogenLineShape(object):
         #     wavelengths_doppler_num += 1
 
         self.wavelengths_doppler = linspace(self.cwl-10, self.cwl+10, wavelengths_doppler_num)
-        self.doppler_function = DopplerLine(cwl=self.cwl, wavelengths=self.wavelengths_doppler, atomic_mass=atomic_mass, half_range=5000)
+        self.doppler_function = DopplerLine(cwl=copy(self.cwl), wavelengths=self.wavelengths_doppler, atomic_mass=atomic_mass, half_range=5000)
 
     def __call__(self, theta):
         if self.zeeman:
@@ -292,6 +302,7 @@ class BalmerHydrogenLine(object):
         self.line = self.species+'_'+str(self.cwl)
         self.wavelengths = wavelengths
         self.n_upper = self.plasma.input_dict[self.species][self.cwl]['n_upper']
+        self.n_lower = self.plasma.input_dict[self.species][self.cwl]['n_lower']
         self.atomic_mass = get_atomic_mass(self.species)
         self.los = self.plasma.profile_function.electron_density.x
         self.dl_per_sr = diff(self.los)[0] / (4*pi)
@@ -303,7 +314,7 @@ class BalmerHydrogenLine(object):
         self.exc_pec = self.plasma.hydrogen_pecs[self.line+'_exc']
         self.rec_pec = self.plasma.hydrogen_pecs[self.line+'_rec']
 
-        self.lineshape = HydrogenLineShape(self.cwl, self.reduced_wavelength, self.n_upper, n_lower=2,
+        self.lineshape = HydrogenLineShape(self.cwl, self.reduced_wavelength, self.n_upper, n_lower=self.n_lower,
                                            atomic_mass=self.atomic_mass, zeeman=zeeman)
 
     def __call__(self):
@@ -317,18 +328,12 @@ class BalmerHydrogenLine(object):
 
         rec_pec = np.exp(self.rec_pec(ne, te))
         exc_pec = np.exp(self.exc_pec(ne, te))
-        self.rec_profile = n1*ne*self.dl_per_sr*rec_pec
+        self.rec_profile = n1.clip(1.)*ne*self.dl_per_sr*rec_pec
         self.exc_profile = n0*ne*self.dl_per_sr*nan_to_num(exc_pec)
 
         # set minimum number of photons to be 1
         self.rec_profile.clip(1.)
         self.exc_profile.clip(1.)
-
-        # low_te = 0.2
-        # low_ne = 1e11
-        # indicies = where((te < low_te) | (ne < low_ne))
-        # rec_profile[indicies] = 1
-        # exc_profile[indicies] = 1
 
         rec_sum = self.rec_profile.sum()
         exc_sum = self.exc_profile.sum()

baysar/lineshapes.py
@@ -224,10 +224,12 @@ class MeshLine(object):
             self.empty_theta=np.zeros(len(x)+2)
             self.empty_theta[0]=zero_bounds
             self.empty_theta[-1]=zero_bounds
+            self.x_points=np.concatenate([np.array([min(bounds)]), x, np.array([max(bounds)])])
         else:
             self.empty_theta = np.zeros(len(x))
             self.slice=slice(0, len(self.empty_theta))
-        self.x_points = np.linspace(min(bounds), max(bounds), len(self.empty_theta))
+            self.x_points=x
+
 
         self.check_init()
 
@@ -236,23 +238,14 @@ class MeshLine(object):
             print('self.x_points', self.x_points)
             print('self.empty_theta', self.empty_theta)
             raise ValueError("len(self.x_points) != len(self.empty_theta)")
-        # if self.zero_bounds is None:
-        #     if len(self.x_points) != len(self.empty_theta):
-        #         print('self.x_points', self.x_points)
-        #         print('self.empty_theta', self.empty_theta)
-        #         raise ValueError("len(self.x_points) != len(self.empty_theta)")
-        # else:
-        #     if len(self.x_points) != len(self.empty_theta)-2:
-        #         print('self.x_points', self.x_points)
-        #         print('self.empty_theta', self.empty_theta)
-        #         raise ValueError("len(self.x_points) != len(self.empty_theta)-2")
-
 
     def __call__(self, theta, *args, **kwargs):
-        get_new_profile = interp1d(self.x_points, self.empty_theta, self.kind, bounds_error=False, fill_value='extrapolate')
+        self.empty_theta[self.slice]=theta
         if self.log:
-            return np.power(10, get_new_profile(self.x))
+            get_new_profile = interp1d(self.x_points, np.power(10, self.empty_theta), self.kind, bounds_error=False, fill_value='extrapolate')
+            return get_new_profile(self.x)
         else:
+            get_new_profile = interp1d(self.x_points, self.empty_theta, self.kind, bounds_error=False, fill_value='extrapolate')
             return get_new_profile(self.x)
 
 
@@ -281,18 +274,104 @@ class PlasmaMeshLine(object):
 
         return self.meshprofile(theta)
 
+def bowman_tee_distribution(x, theta):
+    A, x0, sigma0, q, nu, k, f, b = theta
+
+    p0=f*np.tanh(k*(x-x0))
+    sigma=sigma0*np.exp(p0)
+    z=(x-x0)/sigma
+    z=np.power(abs(z), q)
+    p1=np.power((1+z)/nu, -nu)
+
+    return A*p1+b
+
+def bowman_tee_distribution_centred(x, theta):
+    A, sigma0, q, nu, k, f, b = theta
+    return bowman_tee_distribution(x, [A, 0, sigma0, q, nu, k, f, b])
+
+from copy import copy
+
+class BowmanTeeNe(object):
+    def __init__(self, x):
+        self.x=x
+
+    def __call__(self, theta):
+        theta=copy(theta)
+        for i in [0, -1]:
+            theta[i]=np.power(10, theta[i])
+        return bowman_tee_distribution(self.x, theta)
+
+class BowmanTeeTe(object):
+    def __init__(self, x):
+        self.x=x
+
+    def __call__(self, theta):
+        theta=copy(theta)
+        for i in [0, -1]:
+            theta[i]=np.power(10, theta[i])
+        return bowman_tee_distribution_centred(self.x, theta)
+
+from itertools import product
+
+class BowmanTeePlasma(object):
+    def __init__(self, x=None, bounds=None, dr_bounds=[-2, 2], bounds_ne=[11, 16], bounds_te=[-1, 2]):
+        if x is None:
+            self.x=np.linspace(-15, 25, 500)
+        else:
+            self.x=x
+
+        if bounds is None:
+            self.bounds=[[1e-3, 10], [0.1, 10], [1, 2],
+                         [1, 10],  [0.5, 3]]
+        else:
+            self.bounds=bounds
+
+        self.electron_density=BowmanTeeNe(self.x)
+        self.electron_temperature=BowmanTeeTe(self.x)
+
+        self.electron_density.number_of_variables=8
+        self.electron_temperature.number_of_variables=7
+
+        self.construct_bounds(dr_bounds, bounds_ne, bounds_te)
+
+    def construct_bounds(self, dr_bounds,bounds_ne, bounds_te):
+        """
+        A > 0
+        sigma0 > 0
+        0.5 < q < 20
+        nu > 1
+        1 < k < 50
+        1 < f < 10
+        0 < b
+        """
+
+        self.bounds_ne=[bounds_ne, dr_bounds]
+        self.bounds_te=[bounds_te]
+
+        for param, bound in product([self.bounds_ne, self.bounds_te], self.bounds):
+            param.append(bound)
+
+        for param, bound in zip([self.bounds_ne, self.bounds_te], [bounds_ne, bounds_te]):
+            param.append(bound)
+
+        self.electron_density.bounds=self.bounds_ne
+        self.electron_temperature.bounds=self.bounds_te
 
 if __name__=='__main__':
 
-    from tulasa.general import plot
+    from tulasa.general import plot, close_plots
     import time as clock
 
-    # gaussian check
-    x = np.linspace(400, 402, 500)
-    cwl = [401, 401.5]
-    fwhm = 0.2
-    intensity = 1
+    profile_function=BowmanTeePlasma()
 
-    peak = Gaussian(x=x, cwl=cwl, fwhm=fwhm, fractions=None, normalise=True)
+    theta0=np.ones(7)+1
+    theta0[1]=5
+    peaks=[]
+    for i in np.linspace(0., 20, 5): # .5*(np.arange(5)+1):
+        theta0[1]=5
+        theta0[2]=i
+        # theta0[-2]=i
+        print(i, ': ', *theta0)
+        peaks.append(profile_function.electron_temperature(theta0))
 
-    plot(peak(1), x=x)
+    plot(peaks, x=[profile_function.electron_temperature.x for p in peaks], multi='fake') #, log=True)

baysar/plasmas.py
@@ -5,7 +5,7 @@ import copy, warnings
 
 import collections
 from baysar.lineshapes import MeshLine
-from baysar.spectrometers import within
+from baysar.tools import within
 
 from scipy.interpolate import RegularGridInterpolator, RectBivariateSpline
 from adas import run_adas406, read_adf15
@@ -43,14 +43,94 @@ class arb_obj_single_input(object):
     def __call__(self, theta):
         if type(theta) not in (float, int):
             theta = theta[0]
-        return np.power(10, theta)
+        return theta
+
+
+class arb_obj_single_log_input(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        if 'power' not in self.__dict__:
+            self.power=1
+
+    def __call__(self, theta):
+        if type(theta) not in (float, int):
+            theta = theta[0]
+        return np.power(self.power, theta)
+
+# tmp_func = arb_obj_single_input(number_of_variables=1, bounds=[-5, 5])
+class default_calwave_function(object):
+    def __init__(self, number_of_variables=2, bounds=[ [0.9999, 1.0001], [-0.6, 0.6] ]):
+        self.number_of_variables=number_of_variables
+        self.bounds=bounds
+
+        self.check_init()
+
+    def check_init(self):
+        if self.number_of_variables!=len(self.bounds):
+            raise ValueError("self.number_of_variables!=len(self.bounds)")
+
+    def __call__(self, *args):
+        return args[0]
+
+    def calibrate(self, x, theta):
+        m, c=theta
+        return m*x+c
+
+    def inverse_calibrate(self, x, theta):
+        m, c=theta
+        return (x-c)/m
+
+
+class default_cal_function(object):
+    def __init__(self, number_of_variables=1, bounds=[ [-5, 20] ]):
+        self.number_of_variables=number_of_variables
+        self.bounds=bounds
+
+        self.check_init()
+
+    def check_init(self):
+        if self.number_of_variables!=len(self.bounds):
+            raise ValueError("self.number_of_variables!=len(self.bounds)")
+
+    def __call__(self, *args):
+        return power10(args[0])
+
+    # def calibrate(self, x, theta):
+    #     m, c=theta
+    #     return m*x+c
+
+    def inverse_calibrate(self, y, theta):
+        m=theta[0]
+        return y/m
+
+
+class default_background_function(object):
+    def __init__(self, number_of_variables=1, bounds=[ [-5, 20] ]):
+        self.number_of_variables=number_of_variables
+        self.bounds=bounds
+
+        self.check_init()
+
+    def check_init(self):
+        if self.number_of_variables!=len(self.bounds):
+            raise ValueError("self.number_of_variables!=len(self.bounds)")
+
+    def __call__(self, *args):
+        return power10(args[0])
+
+    def calculate_background(self, theta):
+        return theta
+
 
 atomic_number={'He':2, 'Li':3, 'Be':4, 'B':5, 'C':6, 'N':7, 'O':8, 'F':9, 'Ne':10}
 def get_number_of_ions(element):
     return atomic_number[element]+1
 
-
-
+def get_meta(element):
+    num=get_number_of_ions(element)
+    meta=np.zeros(num)
+    meta[0]=1
+    return meta
 
 class PlasmaLine():
 
@@ -64,6 +144,8 @@ class PlasmaLine():
     def __init__(self, input_dict, profile_function=None, cal_functions=None, calwave_functions=None, background_functions=None):
 
         'input_dict input has been checked if created with make_input_dict'
+
+        print("Building PlasmaLine object")
 
         self.input_dict = input_dict
         self.num_chords = len(self.input_dict['wavelength_axis'])
@@ -92,7 +174,6 @@ class PlasmaLine():
         self(np.random.rand(self.n_params))
 
     def __call__(self, theta):
-
         return self.update_plasma_state(theta)
 
     def get_impurities(self):
@@ -102,6 +183,29 @@ class PlasmaLine():
             elem = species[:split_index]
             if elem not in self.impurities:
                 self.impurities.append(elem)
+
+    def append_bounds_from_functional(self, bounds, functional):
+        if type(functional.bounds[0])==list:
+            if len(functional.bounds)!=functional.number_of_variables:
+                raise ValueError(type(functional), "len(functional.bounds)!=functional.number_of_variables")
+            [bounds.append(b) for b in functional.bounds]
+        elif np.isreal(functional.bounds[0]):
+            [bounds.append(functional.bounds) for num in np.arange(functional.number_of_variables)]
+        else:
+            raise TypeError("functional.bounds must be a list of len 2 or a list of shape (num_var, 2)")
+
+        if functional.number_of_variables==1 and type(functional.bounds[0]) is not list:
+            check=[functional.bounds]
+        else:
+            check=functional.bounds
+        if check!=bounds[-functional.number_of_variables:]:
+            raise ValueError("functional.bounds!=bounds[-functional.number_of_variables:]",
+                             check!=bounds[-functional.number_of_variables:],
+                             type(functional), type(check), check,
+                             type(bounds[-functional.number_of_variables:]),
+                             bounds[-functional.number_of_variables:])
+
+        return bounds
 
     def build_tags_slices_and_bounds(self):
 
@@ -119,18 +223,19 @@ class PlasmaLine():
             slice_lengths.append(calwave_func.number_of_variables)
             slice_lengths.append(back_func.number_of_variables)
 
-            bounds.append(cal_func.bounds)
-            bounds.append(calwave_func.bounds)
-            bounds.append(back_func.bounds)
+            bounds=self.append_bounds_from_functional(bounds, cal_func)
+            bounds=self.append_bounds_from_functional(bounds, calwave_func)
+            bounds=self.append_bounds_from_functional(bounds, back_func)
 
         self.tags.append('electron_density')
         self.tags.append('electron_temperature')
 
-        slice_lengths.append(self.profile_function.number_of_variables_ne)
-        slice_lengths.append(self.profile_function.number_of_variables_te)
+        slice_lengths.append(self.profile_function.electron_density.number_of_variables)
+        slice_lengths.append(self.profile_function.electron_temperature.number_of_variables)
 
-        [bounds.append(self.profile_function.bounds_ne) for num in np.arange(self.profile_function.number_of_variables_ne)]
-        [bounds.append(self.profile_function.bounds_te) for num in np.arange(self.profile_function.number_of_variables_te)]
+        bounds=self.append_bounds_from_functional(bounds, self.profile_function.electron_density)
+        bounds=self.append_bounds_from_functional(bounds, self.profile_function.electron_temperature)
+
 
         hydrogen_shape_tags = ['b-field', 'viewangle']
         hydrogen_shape_bounds = [[0, 10], [0, 2]]
@@ -142,7 +247,7 @@ class PlasmaLine():
                 bounds.append(b)
 
         impurity_tags = ['_dens', '_Ti', '_tau']
-        impurity_bounds = [[9, 15], [-1, 3], [-6, 3]]
+        impurity_bounds = [[9, 15], [-2, 2], [-6, 2]]
         for tag in impurity_tags:
             for s in self.species:
                 is_h_isotope = any([s[0:2]==h+'_' for h in self.hydrogen_isotopes])
@@ -225,7 +330,7 @@ class PlasmaLine():
             plasma_state.append((p, values))
 
         self.plasma_state = collections.OrderedDict(plasma_state)
-        self.plasma_state['main_ion_density'] = self.plasma_state['electron_density'] - self.calc_total_impurity_electrons(False)
+        self.plasma_state['main_ion_density'] = self.plasma_state['electron_density'] - self.calc_total_impurity_electrons(True)
 
     def get_theta_functions(self, profile_function=None, cal_functions=None, calwave_functions=None, background_functions=None):
 
@@ -240,18 +345,21 @@ class PlasmaLine():
         else:
             self.profile_function = profile_function
 
-        tmp_func = arb_obj_single_input(number_of_variables=1, bounds=[-5, 20])
+        # tmp_func = arb_obj_single_log_input(number_of_variables=1, bounds=[-5, 20], power=10)
+        tmp_func=default_cal_function()
         if cal_functions is None:
             self.cal_functions = [tmp_func for num in np.arange(self.num_chords)]
         else:
             self.cal_functions = cal_functions
 
+        tmp_func=default_background_function()
         if background_functions is None:
             self.background_functions = [tmp_func for num in np.arange(self.num_chords)]
         else:
             self.background_functions = background_functions
 
-        tmp_func = arb_obj_single_input(number_of_variables=1, bounds=[-1e-5, 1e-5])
+        # tmp_func = arb_obj_single_input(number_of_variables=1, bounds=[-5, 5])
+        tmp_func=default_calwave_function()
         if calwave_functions is None:
             self.calwave_functions = [tmp_func for num in np.arange(self.num_chords)]
         else:
@@ -268,9 +376,9 @@ class PlasmaLine():
 
         theta_functions_tuples = []
         function_tags = ['cal', 'back', 'electron_density', 'electron_temperature']
-        function_tags_full = [tag for tag in self.tags if any([check in tag for check in function_tags]) or \
+        function_tags_full = [tag for tag in self.tags if (any([check in tag for check in function_tags]) or \
                                                           any([tag.startswith(s+'_') for s in self.species]) or \
-                                                          'X_' in tag]
+                                                          'X_' in tag)]
 
         self.function_check = [any([check in tag for check in function_tags]) or
                                any([tag.startswith(s+'_') for s in self.species]) or
@@ -285,7 +393,7 @@ class PlasmaLine():
         self.theta_functions = collections.OrderedDict(theta_functions_tuples)
 
     def calc_total_impurity_electrons(self, set_attribute=True):
-        total_impurity_electrons = 0
+        total_impurity_electrons=0
 
         ne = self.plasma_state['electron_density']
         te = self.plasma_state['electron_temperature']
@@ -301,16 +409,15 @@ class PlasmaLine():
             tmp_in = (tau, ne, te)
             average_charge = np.exp(self.impurity_average_charge[species](tmp_in))
 
-            total_impurity_electrons += average_charge * conc
+            total_impurity_electrons+=average_charge*conc
 
         if set_attribute:
             self.total_impurity_electrons = np.nan_to_num(total_impurity_electrons)
-        else:
-            return np.nan_to_num(total_impurity_electrons)
+
+        return np.nan_to_num(total_impurity_electrons)
 
     def build_impurity_average_charge(self):
         interps = []
-
         for species in self.impurity_species:
             interps.append( (species, self.build_impurity_electron_interpolator(species)) )
 
@@ -325,13 +432,16 @@ class PlasmaLine():
         elem, ion = self.species_to_elem_and_ion(species)
         ionp1 = ion + 1
 
-        z_eff = ion*self.impurity_ion_bal[elem][:, :, :, ion] + ionp1*self.impurity_ion_bal[elem][:, :, :, ionp1]
+        z_bal=self.impurity_ion_bal[elem][:, :, :, ion]
+        zp1_bal=self.impurity_ion_bal[elem][:, :, :, ionp1]
+        z_eff=(ion*z_bal+ionp1*zp1_bal) # /(z_bal+zp1_bal)
 
-        return RegularGridInterpolator((tau, ne, te), np.log(z_eff), bounds_error=False)
+        return RegularGridInterpolator((tau, ne, te), np.log(z_eff), bounds_error=False, fill_value=None)
 
     def get_impurity_ion_bal(self):
 
         ion_bals = []
+        rad_power = []
 
         te = self.adas_plasma_inputs['te']
         ne = self.adas_plasma_inputs['ne']
@@ -339,15 +449,21 @@ class PlasmaLine():
 
         for elem in self.impurities:
             num_ions=get_number_of_ions(elem)
-            bal = np.zeros((len(tau), len(ne), len(te), num_ions))
+            shape=(len(tau), len(ne), len(te), num_ions)
+            bal=np.zeros(shape)
+            power=[] # np.zeros(shape)
             for t_counter, t in enumerate(tau):
                 with HiddenPrints():
-                    out, _ = run_adas406(year=96, elem=elem, te=te, dens=ne, tint=t, all=True)
-                bal[t_counter, :, :, :] = out['ion'].clip(1e-50)
+                    meta=get_meta(elem)
+                    out, pow = run_adas406(year=96, elem=elem, te=te, dens=ne, tint=t, meta=meta, all=True)
+                bal[t_counter, :, :, :]=out['ion'].clip(1e-50)
+                power.append(pow)
 
             ion_bals.append((elem, bal))
+            rad_power.append((elem, power))
 
         self.impurity_ion_bal = dict(ion_bals)
+        self.impurity_raditive_power=dict(rad_power)
 
     def build_impurity_tec(self, file, exc, rec, elem, ion):
 
@@ -364,9 +480,10 @@ class PlasmaLine():
 
         tec_splines=[]
         for t_counter, t in enumerate(tau):
-            ionbal = self.impurity_ion_bal[elem]
-            rates = big_ne*(pecs_exc.T*ionbal[t_counter, :, :, ion] +
-                                              pecs_rec.T*ionbal[t_counter, :, :, ion+1])
+            # ionbal = self.impurity_ion_bal[elem]
+            z_bal=self.impurity_ion_bal[elem][t_counter, :, :, ion]
+            zp1_bal=self.impurity_ion_bal[elem][t_counter, :, :, ion+1]
+            rates = big_ne*(pecs_exc.T*z_bal+pecs_rec.T*zp1_bal) # /(z_bal+zp1_bal)
             # tec406[t_counter, :, :] = big_ne*(pecs_exc.T*ionbal[t_counter, :, :, ion] +
             #                                   pecs_rec.T*ionbal[t_counter, :, :, ion+1])
             tec_splines.append(RectBivariateSpline(ne, te, np.log(rates.clip(1e-50))).ev)
@@ -380,6 +497,7 @@ class PlasmaLine():
         tecs = []
         for species in self.impurity_species:
             for line in self.input_dict[species].keys():
+                print("Building impurity TEC splines: {} {}".format(species, line, end="\r"))
                 line_str = str(line).replace(', ', '_')
                 for bad_character in ['[', ']', '(', ')']:
                     line_str=line_str.replace(bad_character, '')
@@ -407,6 +525,7 @@ class PlasmaLine():
         pecs = []
         for species in self.hydrogen_species:
             for line in self.input_dict[species].keys():
+                print("Building hydrogen PEC splines: {} {}".format(species, line, end="\r"))
                 line_tag = species+'_'+str(line).replace(', ', '_')
                 file = self.input_dict[species][line]['pec']
                 exc = self.input_dict[species][line]['exc_block']

baysar/posterior.py
@@ -6,7 +6,8 @@ from scipy.optimize import fmin_l_bfgs_b
 
 from baysar.priors import CurvatureCost
 from baysar.plasmas import PlasmaLine
-from baysar.spectrometers import SpectrometerChord, within, progressbar, clip_data
+from baysar.spectrometers import SpectrometerChord
+from baysar.tools import within, progressbar, clip_data
 from baysar.linemodels import XLine
 
 '''
@@ -65,15 +66,7 @@ class BaysarPosterior(object):
         self.positive_thetas = []
         self.runtimes = []
 
-        start=self.random_start()
-        run_check=self(start)
-        if not (np.isreal(run_check) and -1e50 < run_check and run_check < 0):
-            if skip_test:
-                ValueError("Posterior is not evalauating correctly. logP =", run_check, 'from input=', start)
-            else:
-                raise ValueError("Posterior is not evalauating correctly. logP =", run_check, 'from input=', start)
-
-        print("Posterior successfully created!") #instantiated
+        self.init_test(skip_test)
 
     def check_inputs(self, priors, check_bounds, temper, curvature, print_errors):
         if len(priors)>0:
@@ -97,13 +90,28 @@ class BaysarPosterior(object):
         if type(print_errors) is not bool:
             raise TypeError("print_errors must be True or False and is False by default")
 
-    def __call__(self, theta, skip_error=True):
+    def init_test(self, skip_test):
+        start=self.random_start()
+        run_check=self(start, True)
+        if not (np.isreal(run_check) and -1e50 < run_check and run_check < 0):
+            if skip_test:
+                ValueError("Posterior is not evalauating correctly. logP =", run_check, 'from input=', start)
+            else:
+                raise ValueError("Posterior is not evalauating correctly. logP =", run_check, 'from input=', start)
+        print("Posterior successfully created!") #instantiated
+
+    def __call__(self, theta, skip_error=False):
         theta=list(theta)
         self.last_proposal=theta
+        # print('saved last theta')
         # updating plasma state
         self.plasma(theta)
+        # print('updated plasma')
         prob = sum( p() for p in self.posterior_components )
-        self.check_output(prob)
+        # print('prob calculated')
+        if not skip_error:
+            self.check_output(prob)
+        # print(prob)
         return prob/self.temper # temper default is 1
 
     def check_output(self, prob):
@@ -139,7 +147,8 @@ class BaysarPosterior(object):
         half_width_range=np.array([-half_width_range, half_width_range])
         for chord in chords:
             mean=np.log10(np.mean(chord.y_data_continuum)) # std=np.std(chord.y_data_continuum)/10
-            start[self.plasma.slices['background'+str(chord.chord_number)].start]=np.random.uniform(*(mean+half_width_range))
+            prandom_background=np.random.uniform(*(mean+half_width_range))
+            start[self.plasma.slices['background'+str(chord.chord_number)].start]=prandom_background
             # improved start for mystery_lines
             for xline in [line for line in chord.lines if type(line)==XLine]:
                 estemate_ems=0
@@ -153,21 +162,45 @@ class BaysarPosterior(object):
 
     def random_sample(self, number=1, order=1, flat=False):
         sample=[]
-        for rstart in progressbar(np.arange(number), 'Building random sample: ', 30):
-            sample.append(self.random_start(order, flat))
-        # sample_and_prob=[]
-        # for s in progressbar(sample, 'Sorting from high to low probability: ', 30):
-        #     sample_and_prob.append((self.cost(s), s))
-        print('Sorting random sample from high to low probability')
-        return sorted(sample, key=lambda x:self.cost(x))
+        for _ in progressbar(np.arange(number), 'Building random sample: ', 30):
+            rstart=self.random_start(order, flat)
+            sample.append((self(rstart), rstart))
+        if not all(np.isreal([s[0] for s in sample])):
+            raise ValueError("Blah")
+
+        sample=sorted(sample, key=lambda x:-x[0])
+        return [s[1] for s in sample]
 
     def sample_start(self, number, scale=1, order=1, flat=False):
         sample=[]
-        random_sample=self.random_sample(number*scale, order, flat)[:number]
+        num_failed_grad_opt=0
+        random_sample=self.random_sample(scale, order, flat)[:number]
         for rstart in progressbar(random_sample, 'Building starting sample: ', 30):
-            start, logp, info=fmin_l_bfgs_b(self.cost, rstart, approx_grad=True, bounds=self.plasma.theta_bounds.tolist())
-            sample.append((start, logp))
-        return [s[0].tolist() for s in sorted(sample, key=lambda x:x[1])]
+            # this try loop is not ideal
+            # but is needed as parameters out of the bounds are sometimes being sampled and breaking the code
+            # (its the taus)
+            try:
+                start, logp, info=fmin_l_bfgs_b(self.cost, rstart, approx_grad=True, bounds=self.plasma.theta_bounds.tolist())
+                sample.append((-logp, start))
+            except:
+                num_failed_grad_opt+=1
+                if num_failed_grad_opt==1:
+                    self.broke_grad_opt=[self.last_proposal]
+                else:
+                    self.broke_grad_opt.append(self.last_proposal)
+
+        if num_failed_grad_opt==number:
+            raise ValueError("{} % of the gradient optimisation failed!".format(np.round(100.0, 1)))
+        elif num_failed_grad_opt>0:
+            pc=np.round(100*(1-num_failed_grad_opt/number), 2)
+            self.grad_opt_performance='{} out of {} failed the gradient optimisation ({} % succeeded)'.format(num_failed_grad_opt, number, pc)
+            print(self.grad_opt_performance)
+        else:
+            self.grad_opt_performance='{} % of the gradient optimisation succeeded!'.format(np.round(100.0, 1))
+
+        sample=sorted(sample, key=lambda x:-x[0])
+        return [s[1] for s in sample]
+
 
 
 from copy import copy

baysar/priors.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 def curvature(profile):
     grad1 = np.gradient(profile / max(profile))
@@ -68,6 +68,123 @@ class FlatnessPrior(object):
     def __call__(self):
         self.empty_array[self.slice]=self.plasma.plasma_theta.get(self.tag)
         return flatness_prior(self.empty_array, self.scale)
+
+class SeparatrixTePrior(object):
+    def __init__(self, scale, plasma, index):
+        self.scale=power(10, scale)
+        self.plasma=plasma
+        self.separatrix_position=index
+
+    def __call__(self):
+        te=self.plasma.plasma_theta['electron_temperature']
+        te_check=te-max(te)
+        return self.scale*te_check[self.separatrix_position]
+
+class WallConditionsPrior:
+    def __init__(self, scale, plasma, ne=None):
+        self.scale=scale # power(10, scale)
+        self.plasma=plasma
+        self.ne_scale=ne
+
+    def __call__(self):
+        te=self.plasma.plasma_state['electron_temperature']
+        cost=self.scale*(te[0]+te[-1])
+        if self.ne_scale is not None:
+            ne=np.log10( self.plasma.plasma_state['electron_density'] )
+            cost+=self.ne_scale*abs(ne[0]+ne[1])
+        return -cost
+
+from numpy import zeros, diag, eye, log, exp, subtract
+from scipy.linalg import solve_banded, solve_triangular, ldl
+
+def tridiagonal_banded_form(A):
+    B = zeros([3, A.shape[0]])
+    B[0, 1:] = diag(A, k = 1)
+    B[1, :] = diag(A)
+    B[2,:-1] = diag(A, k = -1)
+    return B
+
+
+
+
+class SymmetricSystemSolver(object):
+    def __init__(self, A):
+        L_perm, D, fwd_perms = ldl(A)
+
+        self.L = L_perm[fwd_perms,:]
+        self.DB = tridiagonal_banded_form(D)
+        self.fwd_perms = fwd_perms
+        self.rev_perms = fwd_perms.argsort()
+
+    def __call__(self, b):
+        # first store the permuted form of b
+        b_perm = b[self.fwd_perms]
+
+        # solve the system by substitution
+        y = solve_triangular(self.L, b_perm, lower = True)
+        h = solve_banded((1, 1), self.DB, y)
+        x_perm = solve_triangular(self.L.T, h, lower = False)
+
+        # now un-permute the solution vector
+        x_sol = x_perm[self.rev_perms]
+        return x_sol
+
+
+
+
+class GaussianProcessPrior(object):
+    def __init__(self, mu, plasma = None, A = 2., L = 0.02, profile=None):
+        self.plasma = plasma
+        self.tag=profile
+        self.A = A
+        self.L = L
+
+        self.mu = mu
+        self.los=plasma.profile_function.electron_density.x_points
+        self.empty_array=plasma.profile_function.electron_density.empty_theta
+        if plasma.profile_function.electron_density.zero_bounds is not None:
+            self.slice=slice(1, -1)
+        else:
+            self.slice=slice(0, len(self.empty_array))
+
+
+        # get the squared-euclidean distance using outer subtraction
+        D = subtract.outer(self.los, self.los)**2
+
+        # build covariance matrix using the 'squared-exponential' function
+        K = (self.A**2)*exp(-0.5*D/self.L**2)
+
+        # construct the LDL solver for the covariance matrix
+        solver = SymmetricSystemSolver(K)
+
+        # use the solver to get the inverse-covariance matrix
+        I = eye(K.shape[0])
+        self.iK = solver(I)
+
+        covariance_error=abs(self.iK.dot(K)-I).max()
+        tolerence=1e-6
+        if covariance_error > tolerence:
+            # raise ValueError("Error in construction of covariance matrix!"+
+            #                 f"max(|iKK-I|) = {covariance_error:g0.3} (> {tolerence})")
+            raise ValueError("Error in construction of covariance matrix! \n \
+                              max(|iKK-I|) = {} > {}".format(covariance_error, tolerence))
+
+    def __call__(self):
+        self.empty_array[self.slice]=self.plasma.plasma_theta.get(self.tag)
+        return  self.log_prior( self.empty_array - self.mu )
+
+    def log_prior(self, field):
+        return -0.5*(field.T).dot( self.iK.dot(field) )
+
+    # def gradient(self):
+    #     grad = zeros(self.plasma.N_params)
+    #     grad[self.plasma.slices['Te']] = self.log_prior_gradient( log(self.plasma.get('Te')) - self.mu_Te ) / self.plasma.get('Te')
+    #     grad[self.plasma.slices['ne']] = self.log_prior_gradient( log(self.plasma.get('ne')) - self.mu_ne ) / self.plasma.get('ne')
+    #     # grad[self.plasma.slices['n0']] = self.log_prior_gradient( log(self.plasma.get('n0')) - self.mu_n0 ) / self.plasma.get('n0')
+    #     return grad
+
+    # def log_prior_gradient(self, field):
+    #     return -self.iK.dot(field)
 
 if __name__=='__main__':
     pass

baysar/spectrometers.py
@@ -1,5 +1,6 @@
 from numpy import square, sqrt, mean, linspace, nan, log, diff, arange, zeros, concatenate, where
 
+from scipy.ndimage.measurements import center_of_mass
 from scipy.interpolate import interp1d
 from scipy.signal import fftconvolve
 from scipy import sparse
@@ -11,61 +12,7 @@ import os, sys, io, warnings
 
 from baysar.linemodels import XLine, ADAS406Lines, BalmerHydrogenLine
 from baysar.lineshapes import Gaussian
-
-def clip_data(x_data, y_data, x_range):
-
-    '''
-    returns sections of x and y data that is within the desired x range
-
-    :param x_data:
-    :param y_data:
-    :param x_range:
-    :return:
-    '''
-
-    assert len(x_data)==len(y_data), 'len(x_data)!=len(y_data)'
-
-    x_index = where((min(x_range) < x_data) & (x_data < max(x_range)))
-
-    if not np.isreal(x_data[x_index]).all():
-        print('not isreal(x_data[x_index])')
-        print(x_data)
-    if not np.isreal(y_data[x_index]).all():
-        print('not isreal(Y_data[x_index])')
-        print(y_data)
-
-    return x_data[x_index], y_data[x_index]
-
-def progressbar(it, prefix="", size=60, file=sys.stdout):
-    count = len(it)
-    def show(j):
-        x = int(size*j/count)
-        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
-        file.flush()
-    show(0)
-    for i, item in enumerate(it):
-        yield item
-        show(i+1)
-    file.write("\n")
-    file.flush()
-
-from baysar.input_functions import within
-from scipy.ndimage.measurements import center_of_mass
-
-def centre_peak0(peak, places=7):
-    com=center_of_mass(peak)
-    x=np.arange(len(peak))
-    centre=np.mean(x)
-    shift=com-centre
-    interp=interp1d(x, peak, bounds_error=False, fill_value=0.)
-    return interp(x+shift)
-
-def centre_peak(peak, places=7):
-    x=np.arange(len(peak))
-    centre=np.mean(x)
-    while not np.round(abs(centre-center_of_mass(peak))[0], places)==0:
-        peak=centre_peak0(peak)
-    return peak
+from baysar.tools import clip_data, progressbar, centre_peak
 
 class SpectrometerChord(object):
 
@@ -77,6 +24,7 @@ class SpectrometerChord(object):
     """
 
     def __init__(self, plasma, refine=None, chord_number=None):
+        print("Building SpectrometerChord no. %d"%(chord_number))
         self.chord_number = chord_number
         self.plasma = plasma
         self.input_dict = self.plasma.input_dict
@@ -84,9 +32,10 @@ class SpectrometerChord(object):
         self.y_data = self.input_dict['experimental_emission'][self.chord_number]
         self.x_data = self.input_dict['wavelength_axis'][self.chord_number]
         if self.refine is not None:
-            assert type(self.refine) in (int, float), 'self.refine is not a int or float'
-            low, up = min(self.x_data), max(self.x_data)
-            self.x_data_fm=linspace(low, up, int( abs(low - up) / self.refine ))
+            if not np.isreal(self.refine):
+                raise TypeError('self.refine is not a real number')
+            low, up = self.x_data.min(), self.x_data.max()+self.refine
+            self.x_data_fm=arange(low, up, self.refine)
         else:
             self.x_data_fm=self.x_data
 
@@ -95,12 +44,16 @@ class SpectrometerChord(object):
         self.dispersion_ratios=self.dispersion_fm/self.dispersion
         self.instrument_function=centre_peak(self.input_dict['instrument_function'][self.chord_number])
         self.instrument_function=np.true_divide(self.instrument_function, self.instrument_function.sum())
-        self.noise_region = self.input_dict['noise_region'][self.chord_number]
-        self.a_cal = self.input_dict['emission_constant'][self.chord_number]
+        self.noise_region=self.input_dict['noise_region'][self.chord_number]
+        self.a_cal=self.input_dict['emission_constant'][self.chord_number]
+        self.wavelength_calibrator=self.plasma.calwave_functions[self.chord_number]
+        self.radiance_calibrator=self.plasma.cal_functions[self.chord_number]
+        self.background_function=self.plasma.background_functions[self.chord_number]
 
-        self.int_func_sparce_matrix()
         self.get_error()
         self.get_lines()
+        self.int_func_sparce_matrix()
+        print("Built SpectrometerChord no. %d"%(chord_number))
 
     def __call__(self, *args, **kwargs):
         return self.likelihood()
@@ -124,36 +77,55 @@ class SpectrometerChord(object):
         else:
             likeli=-0.5*sum(((self.y_data-fm)/self.error)**2)
         assert likeli != nan, 'likeli == nan'
+
         return likeli
 
     def forward_model(self):
-        wave_cal = self.plasma.plasma_state['calwave'+str(self.chord_number)]
-        self.wavelength_scaling(wave_cal)
+        self.wavelength_scaling()
         spectra = sum([l().flatten() for l in self.lines]) # TODO: This is what takes all the time?
-        self.wavelength_scaling(1/wave_cal)
-        continuum=self.plasma.plasma_state['background'+str(self.chord_number)]
-        tmp_a_cal=self.plasma.plasma_state['cal'+str(self.chord_number)]
-        spectra=(spectra/tmp_a_cal)+continuum
+        self.wavelength_scaling(inverse=True)
+
+        background_theta=self.plasma.plasma_state['background'+str(self.chord_number)]
+        continuum=self.background_function.calculate_background(background_theta)
+
+        ems_cal_theta=self.plasma.plasma_state['cal'+str(self.chord_number)]
+        spectra0=self.radiance_calibrator.inverse_calibrate(spectra, ems_cal_theta)
 
         # TODO - BIG SAVINGS BOGOF
-        return self.instrument_function_matrix.dot(spectra)
+        # TODO - centre of mass check?
+        return self.instrument_function_matrix.dot(spectra)+continuum
 
-    def wavelength_scaling(self, wave_cal):
+        # spectra=fftconvolve(spectra0, self.instrument_function_fm, mode='same') # needs to be the interpolated int_func ?
+        # sinterp=interp1d(self.x_data_fm, spectra)
+        # return sinterp(self.x_data)
+
+    def wavelength_scaling(self, inverse=False):
+        cal_theta=self.plasma.plasma_state['calwave'+str(self.chord_number)]
+        if inverse:
+            calibrate=self.wavelength_calibrator.inverse_calibrate
+        else:
+            calibrate=self.wavelength_calibrator.calibrate
+
         for line in self.lines:
             if type(line)==XLine:
-                line.line.x*=wave_cal
+                line.line.cwl=calibrate(line.line.cwl, cal_theta)
             if type(line)==ADAS406Lines:
-                line.linefunction.line.x*=wave_cal
+                line.linefunction.line.cwl=calibrate(line.linefunction.line.cwl, cal_theta)
             if type(line)==BalmerHydrogenLine:
-                line.lineshape.wavelengths*=wave_cal
-                line.lineshape.doppler_function.line.x*=wave_cal
+                line.lineshape.cwl=calibrate(line.lineshape.cwl, cal_theta)
 
     def get_lines(self):
+        # print("Getting line objects")
         self.lines = []
         for species in self.input_dict['species']:
-            index = np.where([s=='_' for s in species])[0][0]
-            elem = species[:index]
+            index=np.where([s=='_' for s in species])[0][0]
+            elem=species[:index]
             for l in self.input_dict[species]:
+                # sys.stdout.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+                sys.stdout.write("Getting line objects: {0} {1}".format(species, l, end="\r"))
+                sys.stdout.flush()
+                sys.stdout.write("\n")
+                sys.stdout.flush()
                 if any([elem==h for h in self.plasma.hydrogen_isotopes]):
                     line = BalmerHydrogenLine(self.plasma, species, l, self.x_data_fm)
                 else:
@@ -162,6 +134,10 @@ class SpectrometerChord(object):
 
         if 'X_lines' in self.input_dict:
             for l, f in zip(self.input_dict['X_lines'], self.input_dict['X_fractions']):
+                sys.stdout.write("Getting XLine objects: {0}".format(l, end="\r"))
+                sys.stdout.flush()
+                sys.stdout.write("\n")
+                sys.stdout.flush()
                 line = XLine(plasma=self.plasma, species='X', cwl=l, fwhm=diff(self.x_data)[0],
                              wavelengths=self.x_data_fm, fractions=f)
                 self.lines.append(line)
@@ -179,18 +155,39 @@ class SpectrometerChord(object):
                                                   len_instrument_function)
 
         int_func_interp = interp1d(self.instrument_function_x, self.instrument_function, bounds_error=False, fill_value=0.)
+
+        if_x_fm_res=self.dispersion_ratios*np.diff(self.instrument_function_x)[0]
+        self.instrument_function_x_fm=arange(self.instrument_function_x.min(),
+                                            self.instrument_function_x.max(), if_x_fm_res)
+        self.instrument_function_fm=int_func_interp(self.instrument_function_x_fm)
+        self.instrument_function_fm/=self.instrument_function_fm.sum() # /self.refine)
+
         fine_axis = linspace(0, len(self.x_data), len(self.x_data_fm))
         shape = (len(self.x_data), len(self.x_data_fm))
         matrix = zeros(shape)
         for i in progressbar(np.arange(len(self.x_data)), 'Building convolution matrix: ', 30):
             matrix_i = int_func_interp(fine_axis - i)
-            k = sum(matrix_i)
-            if k == 0:
-                k = 1
-            matrix[i, :] = matrix_i / k
+            matrix[i, :] = matrix_i / sum(matrix_i)
+            # k = sum(matrix_i)
+            # if k == 0:
+            #     matrix[i, :] = matrix_i
+            # else:
+            #     matrix[i, :] = matrix_i / k
 
-        self.instrument_function_matrix = sparse.csc_matrix(self.dispersion_ratios*matrix)
+        self.instrument_function_matrix=sparse.csc_matrix(self.dispersion_ratios*matrix)
         self.instrument_function_matrix.eliminate_zeros()
+        self.check_instrument_function_matrix(self.instrument_function_matrix)
+
+    @classmethod
+    def check_instrument_function_matrix(self, matrix, accuracy=7):
+        tmp0=np.ones(matrix.todense().shape[0])
+        tmp1=np.ones(matrix.todense().shape[1])
+        target=center_of_mass(tmp0)[0]
+        shot=center_of_mass(matrix.dot(tmp1))[0]
+        shift=target-shot
+        if np.round(abs(shift), accuracy):
+            raise ValueError("Instrumental function is not centred", shot, target)
+
 
 
 
@@ -237,6 +234,3 @@ if __name__=='__main__':
     plasma(theta)
     chord=SpectrometerChord(plasma, refine=0.05, chord_number=0)
     print(chord())
-
-    print(chord.instrument_function)
-    print(chord.instrument_function_matrix.todense())