8da980e440cddbef7a63eff2117ea4288d2a89c3
dgahle <daljeet.gahle@strath.ac.uk>
2020-09-29 20:13:39 +0100

    Debug (syntax error): missing bracket in isclose

--- baysar/spectrometers.py
+++ baysar/spectrometers.py
@@ -202,7 +202,7 @@ class SpectrometerChord(object):
 
         self.postconv_integral=np.trapz(spectra, self.x_data_wavecal_interp)
         self.conv_integral_ratio=self.postconv_integral/self.preconv_integral
-        if not np.isclose(self.conv_integral_ratio, 1, rtol=self.convolution_tolerence:
+        if not np.isclose(self.conv_integral_ratio, 1, rtol=self.convolution_tolerence):
             raise ValueError(f"Area not conserved in convolution! ({self.conv_integral_ratio}!=1), {self.dispersion_ratios}, {instrument_function_last_used.sum()}")
 
         spectra+=background
