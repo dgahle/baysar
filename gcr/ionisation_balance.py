ad92fa5f69bdd8aeee7a8fd4c6e972b92483b7b8
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-05-08 11:49:31 +0100

    Calculated non-normalised fractional abundence.

--- gcr/ionisation_balance.py
+++ gcr/ionisation_balance.py
@@ -19,41 +19,44 @@ def ionisation_balance(element: str) -> DataArray:
     shape: tuple[int] = (*scd.shape[1:], proton_number, proton_number)
     rate_matrix: ndarray = zeros(shape)
     charge: int
-    for charge in range(proton_number - 1):
-        scd_block: int = 1 + charge
+    for charge in range(proton_number):
+        scd_block: int = charge if 1 + charge == proton_number else 1 + charge
+        acd_data: ndarray = 0 if charge == 0 else acd.sel(block=charge).data
+        scd_data: ndarray = scd.sel(block=scd_block).data
+        source_data: ndarray = scd_data + acd_data
         # Neutral
         if charge == 0:
             rate_matrix[:, :, charge, charge] = - scd.sel(block=scd_block).data
         # Final entry check
-        elif charge == proton_number:
-            rate_matrix[:, :, charge, charge] = - acd.sel(block=charge).data
-        else:
-            acd_data: ndarray = acd.sel(block=charge).data
-            scd_data: ndarray = scd.sel(block=scd_block).data
+        elif charge == (proton_number - 1):
             # Sources (off diagonal)
-            source_data: ndarray = scd_data + acd_data
+            rate_matrix[:, :, charge, charge - 1] = source_data
+            rate_matrix[:, :, charge - 1, charge] = source_data
+            # Losses (diagonal)
+            rate_matrix[:, :, charge, charge] = - acd_data
+        else:
+            # Sources (off diagonal)
             rate_matrix[:, :, charge, charge - 1] = source_data
             rate_matrix[:, :, charge - 1, charge] = source_data
             # Losses (diagonal)
             rate_matrix[:, :, charge, charge] = - source_data
 
     # Solve for eigenvalues
-    meta: ndarray = zeros(shape[::-1])
-    meta[:, :, 0] = 1.
-    from numpy.linalg import eigvals, solve
-    fractional_abundance: ndarray = eigvals(rate_matrix)
+    from numpy.linalg import eig
+    eigenvalues, eigenvectors = eig(rate_matrix)
+    # Normalise
+    _eigenvalues_normalised: ndarray = eigenvalues - eigenvalues.min()
+    eigenvalues_normalised: ndarray = _eigenvalues_normalised / _eigenvalues_normalised.sum(-1)[:, :, None]
+    # Format
     fractional_abundance: DataArray = DataArray(
-        fractional_abundance,
+        eigenvalues_normalised,
         coords=dict(
             ne=scd.ne,
             Te=scd.Te,
             charge=[0, 1]
         )
     )
-    # TODO: numpy.linalg.LinAlgError: Singular matrix
-    # Solve matrix using the SVD method
-    # Build output DataArray
-    raise NotImplementedError
+    return fractional_abundance
 
 
 def main() -> None:
