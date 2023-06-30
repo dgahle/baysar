9942f7abcf9f5a350dea37d361f32cf6bb627c54
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-06-30 21:17:17 +0100

    Added He and Ne tests for test_ionisation_balance.py

--- tests/gcr/test_ionisation_balance.py
+++ tests/gcr/test_ionisation_balance.py
@@ -5,17 +5,35 @@ from xarray import DataArray, load_dataarray
 from gcr import ionisation_balance
 
 # Variables
+DATA_FOLDER: str = "/Users/daljeet-singh-gahle/github/baysar/tests/gcr/data"
 
 
 # Functions and classes
-def test_ionisation_balance() -> None:
-    element: str = 'h'
-    fractional_abundance: DataArray = ionisation_balance(element)
-    # Test plotting lines
-    # h_ionisation_balance: Path = Path(__file__).parent / "h_ionisation_balance_test_reference.nc"
-    h_ionisation_balance: str = "/Users/daljeet-singh-gahle/github/baysar/tests/gcr/h_ionisation_balance_test_reference.nc"
-    reference: DataArray = load_dataarray(h_ionisation_balance)
-    assert fractional_abundance.interp(ne=1e14).equals(reference)
+class TestIonisationBalance:
+
+    def test_h_ionisation_balance(self) -> None:
+        element: str = 'h'
+        fractional_abundance: DataArray = ionisation_balance(element)
+        # Unit test
+        data_path: str = f"{DATA_FOLDER}/{element}_ionisation_balance_test_reference.nc"
+        reference: DataArray = load_dataarray(data_path)
+        assert fractional_abundance.interp(ne=1e14).equals(reference)
+
+    def test_he_ionisation_balance(self) -> None:
+        element: str = 'he'
+        fractional_abundance: DataArray = ionisation_balance(element)
+        # Unit test
+        data_path: str = f"{DATA_FOLDER}/{element}_ionisation_balance_test_reference.nc"
+        reference: DataArray = load_dataarray(data_path)
+        assert fractional_abundance.interp(ne=1e14).equals(reference)
+
+    def test_ne_ionisation_balance(self) -> None:
+        element: str = 'ne'
+        fractional_abundance: DataArray = ionisation_balance(element)
+        # Unit test
+        data_path: str = f"{DATA_FOLDER}/{element}_ionisation_balance_test_reference.nc"
+        reference: DataArray = load_dataarray(data_path)
+        assert fractional_abundance.interp(ne=1e14).equals(reference)
 
 
 def main() -> None:
