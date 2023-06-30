5a0d7806b6118a1f0ed75141acce5769612eea75
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-06-30 20:08:18 +0100

    Added unittest for H ionisation balance.

--- tests/gcr/test_ionisation_balance.py
+++ tests/gcr/test_ionisation_balance.py
@@ -1,4 +1,7 @@
 # Imports
+from pathlib import Path
+from xarray import DataArray, load_dataarray
+
 from gcr import ionisation_balance
 
 # Variables
@@ -6,12 +9,18 @@ from gcr import ionisation_balance
 
 # Functions and classes
 def test_ionisation_balance() -> None:
-    _ = ionisation_balance()
-    assert False
+    element: str = 'h'
+    fractional_abundance: DataArray = ionisation_balance(element)
+    # Test plotting lines
+    # h_ionisation_balance: Path = Path(__file__).parent / "h_ionisation_balance_test_reference.nc"
+    h_ionisation_balance: str = "/Users/daljeet-singh-gahle/github/baysar/tests/gcr/h_ionisation_balance_test_reference.nc"
+    reference: DataArray = load_dataarray(h_ionisation_balance)
+    assert fractional_abundance.interp(ne=1e14).equals(reference)
+
 
 def main() -> None:
     pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     pass
