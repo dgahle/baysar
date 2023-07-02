295c9101b7e83dccf0ac0218d5e33412b9c8251a
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-07-02 21:34:11 +0100

    Start of a Balmer Series BaySAR demo.

---
+++ demo/balmer_series.py
@@ -0,0 +1,15 @@
+# Imports
+from baysar.posterior import BaysarPosterior
+
+
+# Variables
+
+
+# Functions
+def main() -> None:
+    posterior: BaysarPosterior()
+    pass
+
+
+if __name__ == "__main__":
+    main()
