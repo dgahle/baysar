3b98b5c6378d2acb2cd837b585074bbd128ef49e
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-09-08 11:53:47 +0100

    Moved hard coded variables to a config, added self vs self fit, completed zero fit, and added optimsation graphics.

--- demo/balmer_series.py
+++ demo/balmer_series.py
@@ -1,81 +1,137 @@
 # Imports
-from numpy import linspace, ndarray
+from copy import copy
+from json import load
+from pathlib import Path
+
+from inference.mcmc import GibbsChain
+from numpy import array, diff, log10, ndarray
 from numpy.random import normal
+from pandas import DataFrame
 
 from baysar.input_functions import make_input_dict
-from baysar.lineshapes import gaussian
 from baysar.posterior import BaysarPosterior
-
+from tulasa.general import plot
 
 # Variables
 
 
 # Functions
-def main() -> None:
-    num_chords: int = 0
-    num_pixels: int = 1024
-    wavelength_axis: ndarray = linspace(3700, 4500, num_pixels)
-    experimental_emission: ndarray = normal(0, 1e12, num_pixels)
-    instrument_function: ndarray = gaussian(wavelength_axis, wavelength_axis.mean(), 6., 1)
-    emission_constant: float = 1e11
-    noise_region: list[float, float] = [4150., 4250.]
-    species: list[str] = ['H']
-    ions: list[list[str]] = [['0']]
-    mystery_lines = None
+def optimise_posterior(posterior: BaysarPosterior) -> GibbsChain:
+    start: ndarray = posterior.random_start()
+    chain: GibbsChain = GibbsChain(posterior=posterior, start=start)
+    for n_param, bound in enumerate(posterior.plasma.theta_bounds):
+        chain.set_boundaries(n_param, bound)
 
-    input_dict: dict = make_input_dict(
-        num_chords=num_chords,
-        wavelength_axis=[wavelength_axis],
-        experimental_emission=[experimental_emission],
-        instrument_function=[instrument_function],
-        emission_constant=[emission_constant],
-        noise_region=[noise_region],
-        species=species,
-        ions=ions,
-        mystery_lines=mystery_lines,
-        refine=[0.01],
-        ion_resolved_temperatures=False,
-        ion_resolved_tau=False,
+    chain.run_for(minutes=0.5)
+
+    success_criteria: float = -0.5 * sum(
+        [len(_wavelengths) for _wavelengths in posterior.input_dict["wavelength_axis"]]
+    )
+    sucecssful: bool = success_criteria < max(chain.probs)
+
+    tries: int = 0
+    while not sucecssful and tries < 3:
+        tries += 1
+        chain.run_for(minutes=0.5)
+        sucecssful: bool = max(chain.probs) < success_criteria
+
+    if success_criteria:
+        print("Zero fit was successful!")
+    else:
+        print("Zero fit was unsuccessful!")
+
+    return chain
+
+
+def len_slice(index: slice) -> int:
+    step: int = 1 if index.step is None else index.step
+    return (index.stop - index.start) // step
+
+
+def get_param_index(posterior: BaysarPosterior) -> list[str]:
+
+    param_slices: dict = posterior.plasma.slices
+    index: list[str] = []
+    for param in param_slices:
+        param_slice: slice = param_slices[param]
+        num_param_slice: int = len_slice(param_slice)
+        sub_index: list[str] = [
+            param if num_param_slice == 1 else f"{param}#{n}" for n in range(num_param_slice)
+        ]
+        index.extend(sub_index)
+
+    return index
+
+
+def main() -> None:
+    # TODO: Where has H_0_dens gone?!
+    # TODO: Need a check for negative emission spectra!
+
+    baysar_config_path: Path = Path(__file__).parent / "zero_fit_config.json"
+    with open(baysar_config_path, "r") as f:
+        baysar_config = load(f)
+
+    input_dict: dict = (
+        make_input_dict(**baysar_config)
     )
 
     posterior: BaysarPosterior = BaysarPosterior(input_dict=input_dict)
 
-    # # Plot random start
-    # from tulasa.plotting_functions import plot_fit
-    # sample = [posterior.last_proposal]
-    # plot_fit(posterior, sample, size=100, alpha=0.1, ylim=(1e11, 1e+16), error_norm=False, plasma_ref=None,
-    #          filename=None, nitrogen=None, deuterium=None)
+    # Zero fit!
+    chain: GibbsChain = optimise_posterior(posterior)
 
-    # out, big_out = posterior.optimise(
-    #     pop_size=12, num_eras=1, generations=3, threads=12, initial_population=None, random_sample_size=1000,
-    #     random_order=3, perturbation=None, filename=None, plot=False, plasma_reference=None, return_out=True,
-    #     maxiter=100
-    # )
+    # Self vs Self fit!
+    # Reference theta
+    reference_theta: list[float] = copy(chain.mode())
+    theta_slices: dict[str, slice] = posterior.plasma.slices
+    reference_theta[theta_slices["electron_density"]] = [log10(8e13), 0, 3]
+    reference_theta[theta_slices["electron_temperature"]] = [log10(5.), 3, 1]
+    reference_theta[theta_slices["H_0_tau"]] = [-8.0]
+    reference_theta[theta_slices["H_0_velocity"]] = [0.0]
+    # Create Synthetic Spectra
+    posterior(reference_theta)
+    synthetic_spectra: ndarray = posterior.posterior_components[0].forward_model()
+    # Add noise
+    num_pixels: int = len(synthetic_spectra)
+    wavelength_axis: ndarray = posterior.input_dict['wavelength_axis'][0]
+    synthetic_spectra += normal(0, 1e12, num_pixels)
+    input_dict["experimental_emission"] = [synthetic_spectra]
+    plot(synthetic_spectra, x=wavelength_axis)
+    # Rebuild posterior
+    posterior: BaysarPosterior = BaysarPosterior(input_dict=input_dict)
+    # Fit!
+    chain: GibbsChain = optimise_posterior(posterior)
 
-    # Todo: NaNs in the bounds come from the SpectrometerChord estimation
-    # Todo: NaNs being created in the forward model (also need to check the types going through)
-    # Todo: Why are the lineshape models so much larger than the thesis tagged version?
+    chain.run_for(minutes=5)
 
-    from scipy.optimize import fmin_l_bfgs_b
-    start: ndarray = posterior.random_start()
+    try:
+        chain.plot_diagnostics()
+    except IndexError as index_error:
+        print(f"GibbsChain.plot_diagnostics - {index_error}")
+        plot(chain.probs)
 
-    def callback(theta: list[float]) -> None:
-        print(
-            f"logp = {posterior(theta):.1f} - theta = {theta}"
-        )
+    chain.matrix_plot(reference=reference_theta)
 
-    callback(start)
-
-    x, f, d = fmin_l_bfgs_b(
-        posterior.cost,
-        start,
-        fprime=None,
-        approx_grad=True,
-        bounds=posterior.plasma.theta_bounds.tolist(),
-        m=10, factr=10000000.0, pgtol=1e-05, epsilon=1e-08, iprint=-1,
-        maxfun=15000, maxiter=10, disp=None, callback=callback, maxls=20
+    param_index: list[str] = get_param_index(posterior)
+    data: ndarray = array([reference_theta, chain.mode()]).T
+    data = array([reference_theta, chain.mode(), diff(data).flatten()]).T
+    df_fit: DataFrame = DataFrame(
+        data=data,
+        index=param_index,
+        columns=[
+            "Target",
+            "Fit",
+            "Diff"
+        ]
     )
+    print(df_fit)
 
+    _ = posterior(chain.mode())
+    fit_spectra: ndarray = posterior.posterior_components[0].forward_model()
+    plot(
+        array([synthetic_spectra, fit_spectra]).T,
+        x=array([wavelength_axis, wavelength_axis]).T
+    )
 
 
     pass
