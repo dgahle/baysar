dc00086c37de01009619e059fb756f3ae3ff1b46
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-09-29 15:37:31 +0100

    Reformatted with black and isort. All pytests still run successfully.

--- baysar/plasmas/profile_functions/asymmetric_cauchy.py
+++ baysar/plasmas/profile_functions/asymmetric_cauchy.py
@@ -5,7 +5,9 @@ from numpy import exp, linspace, log, ndarray, power, sqrt, square
 
 
 # Functions
-def asymmetric_cauchy(x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float) -> ndarray:
+def asymmetric_cauchy(
+    x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float
+) -> ndarray:
     # Calculate the amplitude
     A: float = power(10, log_p_max) - p_min
     # Calculate the broadening
@@ -18,7 +20,9 @@ def asymmetric_cauchy(x: ndarray, log_p_max: float, shift: float, sigma: float, 
     return profile
 
 
-def asymmetric_cauchy_d_log_p_max(x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float) -> ndarray:
+def asymmetric_cauchy_d_log_p_max(
+    x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float
+) -> ndarray:
     # Calculate the amplitude
     A: float = log(10) * power(10, log_p_max)
     # Calculate the broadening
@@ -31,32 +35,36 @@ def asymmetric_cauchy_d_log_p_max(x: ndarray, log_p_max: float, shift: float, si
     return gradient_profile
 
 
-def asymmetric_cauchy_d_shift(x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float) -> ndarray:
+def asymmetric_cauchy_d_shift(
+    x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float
+) -> ndarray:
     # Solve dB/dShift by substitution
     dx: ndarray = x - shift
     G: ndarray = 1 + exp(-dx)
-    dB_dG: ndarray = - sigma / square(G)
+    dB_dG: ndarray = -sigma / square(G)
     dG_dshift: ndarray = exp(-dx)
     dB_dshift: ndarray = dB_dG * dG_dshift
     # Solve dC/dShift by quotient rule
     B: ndarray = 0.2 * sigma + sigma / G
-    dC_dshift: ndarray = - (B + dx * dB_dshift) / square(B)
+    dC_dshift: ndarray = -(B + dx * dB_dshift) / square(B)
     # Solve dD/dShift by substitution and quotient rule
     C: ndarray = sigma + dx / B
-    dD_dshift: ndarray = 2 * (dx / C) * - (C + dC_dshift * dx) / square(C)
+    dD_dshift: ndarray = 2 * (dx / C) * -(C + dC_dshift * dx) / square(C)
     # Solve dE/dShift
     D: ndarray = square(dx / C)
     dE_dshift: ndarray = dD_dshift
     # Solve dy/dShift by substitution
     E: ndarray = 1 + D
     A: ndarray = power(10, log_p_max) - p_min
-    dy_dE: ndarray = - A / square(E)
+    dy_dE: ndarray = -A / square(E)
     gradient_profile: ndarray = dy_dE * dE_dshift
 
     return gradient_profile
 
 
-def asymmetric_cauchy_d_sigma(x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float) -> ndarray:
+def asymmetric_cauchy_d_sigma(
+    x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float
+) -> ndarray:
     # Calculate terms needed form the original asymmetric_cauchy function
     dx: ndarray = x - shift
     B: ndarray = 0.2 * sigma + sigma / (1 + exp(-dx))
@@ -67,18 +75,16 @@ def asymmetric_cauchy_d_sigma(x: ndarray, log_p_max: float, shift: float, sigma:
     dB_dsigma: ndarray = 0.2 + 1 / (1 + exp(-dx))
     # Calculate the gradients over the profile
     lhs0: ndarray = sqrt(A) / (1 + D)
-    lhs: ndarray = 2 * power(C, - 3) * square(lhs0 * dx)
-    rhs: ndarray = 1 - (
-        (
-            (x - shift)/square(B)
-        ) * dB_dsigma
-    )
+    lhs: ndarray = 2 * power(C, -3) * square(lhs0 * dx)
+    rhs: ndarray = 1 - (((x - shift) / square(B)) * dB_dsigma)
     gradient_profile: ndarray = rhs * lhs
 
     return gradient_profile
 
 
-def asymmetric_cauchy_d_p_min(x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float) -> ndarray:
+def asymmetric_cauchy_d_p_min(
+    x: ndarray, log_p_max: float, shift: float, sigma: float, p_min: float
+) -> ndarray:
     """
     (d/dpmin)f(x;theta)  = 1 - 1 / (1 + D), where f(x; theta) is the asymmetric_cauchy function.
     """
@@ -92,7 +98,9 @@ def asymmetric_cauchy_d_p_min(x: ndarray, log_p_max: float, shift: float, sigma:
     return gradient_profile
 
 
-def _electron_density(x: ndarray, log_ne_max: float, shift: float, sigma: float) -> ndarray:
+def _electron_density(
+    x: ndarray, log_ne_max: float, shift: float, sigma: float
+) -> ndarray:
     # Calculate the amplitude
     A: float = power(10, log_ne_max)
     # Calculate the broadening
@@ -104,7 +112,9 @@ def _electron_density(x: ndarray, log_ne_max: float, shift: float, sigma: float)
     return profile
 
 
-def _electron_temperature(x: ndarray, log_te_max: float, sigma: float, te_min: float) -> ndarray:
+def _electron_temperature(
+    x: ndarray, log_te_max: float, sigma: float, te_min: float
+) -> ndarray:
     # Calculate the amplitude
     A: float = power(10, log_te_max) - te_min
     # Calculate the broadening
@@ -117,7 +127,6 @@ def _electron_temperature(x: ndarray, log_te_max: float, sigma: float, te_min: f
 
 
 class AsymmetricCauchyProfile:
-
     def __init__(self, x=None):
         self.x: ndarray = linspace(-5, 15, 101) if x is None else x
         pass
--- tests/baysar/plasmas/profile_functions/test_asymmetric_cauchy.py
+++ tests/baysar/plasmas/profile_functions/test_asymmetric_cauchy.py
@@ -3,9 +3,14 @@ from numpy import arange, array, isnan, linspace, log10, ndarray, product
 from pandas import DataFrame, Series
 from scipy.optimize import approx_fprime
 
-from baysar.plasmas.profile_functions.asymmetric_cauchy import asymmetric_cauchy, AsymmetricCauchyProfile
-from baysar.plasmas.profile_functions.asymmetric_cauchy import asymmetric_cauchy_d_log_p_max, asymmetric_cauchy_d_shift
-from baysar.plasmas.profile_functions.asymmetric_cauchy import asymmetric_cauchy_d_sigma, asymmetric_cauchy_d_p_min
+from baysar.plasmas.profile_functions.asymmetric_cauchy import (
+    AsymmetricCauchyProfile,
+    asymmetric_cauchy,
+    asymmetric_cauchy_d_log_p_max,
+    asymmetric_cauchy_d_p_min,
+    asymmetric_cauchy_d_shift,
+    asymmetric_cauchy_d_sigma,
+)
 
 # Variables
 x: ndarray = linspace(-5, 15, 101)
@@ -19,18 +24,18 @@ gradient_functions: list[callable] = [
     asymmetric_cauchy_d_log_p_max,
     asymmetric_cauchy_d_shift,
     asymmetric_cauchy_d_sigma,
-    asymmetric_cauchy_d_p_min
+    asymmetric_cauchy_d_p_min,
 ]
 default_epsilon: float = 1.49e-08
-epsilon: ndarray[float] = array([
-    default_epsilon,
-    default_epsilon,
-    default_epsilon,
-    1e2])
+epsilon: ndarray[float] = array(
+    [default_epsilon, default_epsilon, default_epsilon, 1e2]
+)
 
 
 # Functions and Classes
-def calculate_tolerance(reference: [float, ndarray], rtol: float = 1e-05, atol: float = 1e-08):
+def calculate_tolerance(
+    reference: [float, ndarray], rtol: float = 1e-05, atol: float = 1e-08
+):
     """
     Use the tolerance calculation from numpy.isclose to get a standard deviation to calculate Gaussian errors for
     fractional equivalence tests within machine error.
@@ -50,14 +55,13 @@ def calculate_tolerance(reference: [float, ndarray], rtol: float = 1e-05, atol: 
 
 
 class TestCalculateTolerance:
-
     def test_with_zero(self):
         # Test
-        test: float = calculate_tolerance(reference=0.)
+        test: float = calculate_tolerance(reference=0.0)
         # Reference
         reference: float = 1e-08  # same as atol in the function
         # Check
-        assert test == reference, f'Does not work with reference = {reference}!'
+        assert test == reference, f"Does not work with reference = {reference}!"
 
     def test_with_arange(self):
         # Variables
@@ -67,13 +71,10 @@ class TestCalculateTolerance:
         # Test
         test: ndarray = calculate_tolerance(data)
         # Reference
-        reference: ndarray = array([
-            atol,
-            atol + rtol
-        ])
+        reference: ndarray = array([atol, atol + rtol])
         # Check
         check: ndarray = test == reference
-        assert check.all(), f'Does not work with reference = {reference}!'
+        assert check.all(), f"Does not work with reference = {reference}!"
 
 
 def get_name(var: any) -> str:
@@ -99,99 +100,106 @@ def test_get_name() -> None:
     theta: list[float] = [log_p_max, shift, sigma, p_min]
     test: list[str] = [get_name(var) for var in theta]
     # Reference
-    reference: list[str] = ["log_p_max", "shift", 'sigma', "p_min"]
+    reference: list[str] = ["log_p_max", "shift", "sigma", "p_min"]
     # Check
-    check: list[bool] = [t==r for t, r in zip(test, reference)]
+    check: list[bool] = [t == r for t, r in zip(test, reference)]
     assert_msg: str = ""
     assert all(check), assert_msg
 
 
 class TestAsymmetricCauchy:
-
     def test_peak_value(self) -> None:
         # Variables
         log_p_max: ndarray = array([13, 14, 15])
         # Calculation
-        profile: ndarray = array([
-            asymmetric_cauchy(x, _log_p_max, shift, sigma, p_min) for _log_p_max in log_p_max
-        ])
-        # Checks
-        test: ndarray = log10(
-            profile.max(axis=1)
+        profile: ndarray = array(
+            [
+                asymmetric_cauchy(x, _log_p_max, shift, sigma, p_min)
+                for _log_p_max in log_p_max
+            ]
         )
+        # Checks
+        test: ndarray = log10(profile.max(axis=1))
         checks: bool = (log_p_max - test).sum() == 0.0
         # Assert
-        assert_msg: str = f"Profile peaks are {', '.join([f'{v:.2f}' for v in test])} " \
-                          f"but should be {', '.join([f'{v}' for v in log_p_max])}!"
+        assert_msg: str = (
+            f"Profile peaks are {', '.join([f'{v:.2f}' for v in test])} "
+            f"but should be {', '.join([f'{v}' for v in log_p_max])}!"
+        )
         assert checks, assert_msg
 
     def test_peak_position(self) -> None:
         # Variables
         shift: ndarray = array([-2.0, 0.0, 1.2])
         # Calculation
-        profile: ndarray = array([
-            asymmetric_cauchy(x, log_p_max, _shift, sigma, p_min) for _shift in shift
-        ])
+        profile: ndarray = array(
+            [asymmetric_cauchy(x, log_p_max, _shift, sigma, p_min) for _shift in shift]
+        )
         # Checks
         test: ndarray = x[profile.argmax(1)]
         checks: bool = 1e-9 > (shift - test).sum()
         # Assert
-        assert_msg: str = f"Profile centres are {', '.join([f'{v:.2f}' for v in test])} " \
-                          f"but should be {', '.join([f'{v}' for v in shift])}!"
+        assert_msg: str = (
+            f"Profile centres are {', '.join([f'{v:.2f}' for v in test])} "
+            f"but should be {', '.join([f'{v}' for v in shift])}!"
+        )
         assert checks, assert_msg
 
     def test_peak_min(self) -> None:
         # Variables
         p_min: ndarray = array([-2.0, 0.0, 1.2])
         # Calculation
-        profile: ndarray = array([
-            asymmetric_cauchy(x, log_p_max, shift, sigma, _p_min) for _p_min in p_min
-        ])
+        profile: ndarray = array(
+            [asymmetric_cauchy(x, log_p_max, shift, sigma, _p_min) for _p_min in p_min]
+        )
         # Checks
         test: ndarray = x[profile.argmax(1)]
         checks: bool = 0.0 == (shift - test).sum()
         # Assert
-        assert_msg: str = f"Profile minimum values are {', '.join([f'{v:.2f}' for v in test])} " \
-                          f"but should be {', '.join([f'{v}' for v in p_min])}!"
+        assert_msg: str = (
+            f"Profile minimum values are {', '.join([f'{v:.2f}' for v in test])} "
+            f"but should be {', '.join([f'{v}' for v in p_min])}!"
+        )
         assert checks, assert_msg
 
 
 class TestAsymmetricCauchyGradients:
-
     def test_asymmetric_cauchy_d_log_p_max(self) -> None:
         test: ndarray = asymmetric_cauchy_d_log_p_max(x, log_p_max, shift, sigma, p_min)
         check: ndarray = isnan(test)
-        assert not check.any(), 'asymmetric_cauchy_d_log_p_max is returning NaNs!'
+        assert not check.any(), "asymmetric_cauchy_d_log_p_max is returning NaNs!"
 
     def test_asymmetric_cauchy_d_shift(self) -> None:
         test: ndarray = asymmetric_cauchy_d_shift(x, log_p_max, shift, sigma, p_min)
         check: ndarray = isnan(test)
-        assert not check.any(), 'asymmetric_cauchy_d_shift is returning NaNs!'
+        assert not check.any(), "asymmetric_cauchy_d_shift is returning NaNs!"
 
     def test_asymmetric_cauchy_d_sigma(self) -> None:
         test: ndarray = asymmetric_cauchy_d_sigma(x, log_p_max, shift, sigma, p_min)
         check: ndarray = isnan(test)
-        assert not check.any(), 'asymmetric_cauchy_d_sigma is returning NaNs!'
+        assert not check.any(), "asymmetric_cauchy_d_sigma is returning NaNs!"
 
     def test_asymmetric_cauchy_d_p_min(self) -> None:
         test: ndarray = asymmetric_cauchy_d_p_min(x, log_p_max, shift, sigma, p_min)
         check: ndarray = isnan(test, where=False)
-        assert not check.any(), 'asymmetric_cauchy_d_p_min is returning NaNs!'
+        assert not check.any(), "asymmetric_cauchy_d_p_min is returning NaNs!"
 
     def test_against_approx_fprime(self) -> None:
         # Calculate the gradient using approx_fprime
         profile_function: AsymmetricCauchyProfile = AsymmetricCauchyProfile()
-        reference: ndarray = approx_fprime(theta, profile_function._asymmetric_cauchy, epsilon=epsilon)
+        reference: ndarray = approx_fprime(
+            theta, profile_function._asymmetric_cauchy, epsilon=epsilon
+        )
         # Calculate gratient with local functions
-        test: ndarray = array([
-            f(profile_function.x, *theta) for f in gradient_functions
-        ]).T
+        test: ndarray = array(
+            [f(profile_function.x, *theta) for f in gradient_functions]
+        ).T
         # Check
         error_fraction: ndarray = (test - reference) / calculate_tolerance(reference)
         check: ndarray = abs(error_fraction) < 1.0
         if check.all():
             # Construct a summary DataFrame to present errors
-            ndarray_functions: list[str] = ['mean', 'std', 'min', 'max']
+            ndarray_functions: list[str] = ["mean", "std", "min", "max"]
             error_fraction_reduced_summary: list[ndarray] = [
                 getattr(error_fraction, f)(axis=0) for f in ndarray_functions
             ]
@@ -201,12 +209,14 @@ class TestAsymmetricCauchyGradients:
                 columns=theta_names,
                 index=ndarray_functions,
             )
-            df_error.index.name = 'theta'
+            df_error.index.name = "theta"
             # Write Assert message
             success_pc: float = 100 * check.sum() / product(check.shape)
-            assert_msg: str = f'{100 - success_pc:.2f} % results out of tolerance in the gradient calculation!`\n' \
-                              'Tolerance Error Summary (Acceptable range 0 +/- 1):\n' \
-                              f'{df_error}'
+            assert_msg: str = (
+                f"{100 - success_pc:.2f} % results out of tolerance in the gradient calculation!`\n"
+                "Tolerance Error Summary (Acceptable range 0 +/- 1):\n"
+                f"{df_error}"
+            )
 
             assert check.all(), assert_msg
 
